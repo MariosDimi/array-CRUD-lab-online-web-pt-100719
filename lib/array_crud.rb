0b2409242e111e4300069908175f5afeb664abfc
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-11 16:01:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,33 +7,21 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  puppies = ["Huskies","terrier","poodle","foxs"]
-  puppies << "yorkshire"
 end
 
 def add_element_to_start_of_array(array, element)
-  puppies = ["Huskies","terrier","poodle","foxs"]
-  puppies.unshift("halls")
 end
 
 def remove_element_from_end_of_array(array)
-  puppies = ["Huskies","terrier","poodle","foxs"]
-  foxs = puppies.pop
 end
 
 def remove_element_from_start_of_array(array)
-  puppies = ["Huskies","terrier","poodle","foxs"]
-  halls = puppies.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  puppies = ["Huskies","terrier","poodle","foxs"]
-  puppies[3]
 end
 
 def retrieve_first_element_from_array(array)
-  puppies = ["Huskies","terrier","poodle","foxs"]
-  puppies[0]
 end
 
 def retrieve_last_element_from_array(array)
