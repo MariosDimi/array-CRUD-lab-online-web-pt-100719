ecb3b048d3918f3c1e01411c9e49b2ed299779d7
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-11 16:07:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,17 +8,17 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = ["I","am","really","learning"]
-  array << "arrays"
+  array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  array = ["I","am","really","learning","arrays"]
+  array = ["I","am","really","learning","arrays!"]
   array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  array = ["I","am","really","learning","arrays"]
-  arrays = array.pop
+  array = ["I","am","really","learning","arrays!"]
+  arrays! = array.pop
 end
 
 def remove_element_from_start_of_array(array)
